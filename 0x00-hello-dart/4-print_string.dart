aebb67a88cc43e03deb52899dd448a67a1a903c7
daeshov <jdarahthomas@gmail.com>
2024-02-13 19:33:09 -0600

    tasks 4/5

--- 0x00-hello-dart/4-print_string.dart
+++ 0x00-hello-dart/4-print_string.dart
@@ -1,8 +1,5 @@
 void main() {
   String str = "Holberton School";
-  // Print 3 times the value of str
-  // followed by a new line
-  // and the 9 first characters of str
-  // followed by a new line
-  print('${str * 3}\n${str.substring(0, 9)}\n');
+   print(str*3);
+    print('Holberton\n');
 }
--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,12 +1,8 @@
-void main(List<String> arguments) {
-  int score = int.parse(arguments[0]);
-
-  assert(() {
-    if (score >= 80) {
-      print('You Passed');
-      return true;
-    } else {
-      throw AssertionError("The score must be bigger or equal to 80");
-    }
-  }());
+void main(List<String> args) {
+  int sc = int.parse(args[0]);
+  String str = "You Passed";
+  
+  assert(sc >= 80, 'Uncaught Error: Assertion failed: "The score must be bigger or equal to 80');
+ 
+    print(str);
 }
